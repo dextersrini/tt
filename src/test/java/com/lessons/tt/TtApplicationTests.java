fa58a005a949dfa7b61524e70933bb41a8189484
dextersrini <dextersrini@gmail.com>
2021-12-24 23:02:25 +0530

    New Project

---
+++ src/test/java/com/lessons/tt/TtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessons.tt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
